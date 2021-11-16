b097837574e7c0ed573363c03e521c4817d9af7c
Olartbaraq <akanbi.ges201@gmail.com>
2021-11-16 20:51:38 +0100

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113 :
-        continue
-    print("{:c}".format(i), end='')
+for x in range(97, 123):
+    if (x != 113 and x != 101):
+     print("{:c}".format(x), end="")
