cc9b3981888d28a6b4956da67a7696841172f8e0
Olartbaraq <akanbi.ges201@gmail.com>
2021-11-21 23:21:09 +0100

    identation

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,6 @@ def new_in_list(my_list, idx, element):
         return my_list.copy()
     elif idx > len(my_list) - 1:
         return my_list.copy()
-    elif idx > 0 and idx < len(my_list):
-        for i in range(len(my_list)):
-            copy[idx] = element
-            return copy
+    else:
+        copy[idx] = element
+        return copy
