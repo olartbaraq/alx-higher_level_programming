fc084130e232fe4a4855f07eae15704b421cd50e
Olartbaraq <akanbi.ges201@gmail.com>
2021-11-21 23:59:36 +0100

    concatenation

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    my_new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return my_new_string
+    new_string = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            continue
+        else:
+            new_string += i
+    return new_string
