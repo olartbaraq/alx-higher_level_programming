2d701176216bd6f32857850575d6847049a3f01c
Olartbaraq <akanbi.ges201@gmail.com>
2021-11-23 20:54:03 +0100

    cats op

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,21 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     num = len(sys.argv)
-    operator = ["+", "-", "*", "/"]
+    op = ["+", "-", "*", "/"]
     if num != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] != operator:
+    elif sys.argv[2] != op:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
         a = int(sys.argv[1])
         c = int(sys.argv[3])
         if sys.argv[2] == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
+            print("{} {} {} = {}".format(a, op, b, add(a, b)))
         elif sys.argv[2] == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
+            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
         elif sys.argv[2] == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
+            print("{} {} {} = {}".format(a, op, b, mul(a, b)))
         elif sys.argv[2] == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print("{} {} {} = {}".format(a, op, b, div(a, b)))
