6ccaae76562767a6bfe11b036f85dfa8a77c56b4
Olartbaraq <akanbi.ges201@gmail.com>
2021-12-06 20:15:30 +0100

    use the format method

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,10 @@ def safe_print_list(my_list=[], x=0):
     a = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
-            a=a+1
+            print("{}".format(my_list[i]), end="")
         except IndexError:
             break
+        else:
+            a += 1
     print()
     return a
