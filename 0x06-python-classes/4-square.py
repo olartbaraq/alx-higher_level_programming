9dea29cbf769ae24a91304d97729db7816f6448b
Olartbaraq <akanbi.ges201@gmail.com>
2021-12-08 22:24:57 +0100

    setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""defines a class Square"""
+
+
+class Square:
+    """Represents a Square with its size
+    Attributes:
+    __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """Returns the value of Square Area"""
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """getter method"""
+    return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
