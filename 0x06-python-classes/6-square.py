5fadcaca017509533fa55c1cd85e695b3d3d56ba
Olartbaraq <akanbi.ges201@gmail.com>
2021-12-09 22:46:19 +0100

    update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,10 +69,10 @@ class Square:
         """Prints in standard output
           Square with character # at a position"
         """
-        if self.__size == 0:
+        if self.size == 0:
             print()
         else:
-            print("\n" * self.__position[1], end="")
-            for i in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
+            print("\n" * self.position[1], end="")
+            for i in range(self.size):
+                print(" " * self.position[0], end="")
+                print("#" * self.size)
