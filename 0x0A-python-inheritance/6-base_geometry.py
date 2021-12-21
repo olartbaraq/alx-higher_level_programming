523921795502b1136f58fdccf2535a4bb83d2864
Akanbi Mubaraq <akanbi.ges201@gmail.com>
2021-12-21 02:53:52 -0800

    defines a method with a exception message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+defines a class with instance
+"""
+
+
+class BaseGeometry:
+    """ defines a method and returns a value"""
+
+    def area(self):
+        """ raise an exception when called"""
+        raise Exception("area() is not implemented")
