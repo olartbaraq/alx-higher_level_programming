f57c6eeec93017d95afc6dfd8565c5df8deb3fe0
Akanbi Mubaraq <akanbi.ges201@gmail.com>
2021-12-21 02:08:01 -0800

    return type int

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+instance of a class check 
+"""
+
+
+def is_same_class(obj, a_class):
+    """ returns a bool true if objects is an insy=tance of a class"""
+    return (type(obj) == a_class)
