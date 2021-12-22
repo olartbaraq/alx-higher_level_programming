0f5a29594ffa982ec4d5fbba59dc256503ae7131
Akanbi Mubaraq <akanbi.ges201@gmail.com>
2021-12-22 15:23:34 -0800

    fix docstring

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,5 +10,8 @@ import json module for serializing non string data types
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    returns written file by json module
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         return (json.dump(my_obj, f))
