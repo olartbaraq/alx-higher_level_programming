f20e78d08d021c81594ee082b1481b2044857b21
Akanbi Mubaraq <akanbi.ges201@gmail.com>
2021-12-22 15:21:30 -0800

    using json module to write to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+defines JSON represenation to write files
+"""
+
+import json
+"""
+import json module for serializing non string data types
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        return (json.dump(my_obj, f))
