aef1b04d849df5898c4f6d41da4b2cf198f34dc1
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-01 22:01:58 +0100

    another way to describe a table(update 3)

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -3,4 +3,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
 
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SHOW FULL COLUMNS FROM first_table;
