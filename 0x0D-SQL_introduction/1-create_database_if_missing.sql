5c4ebdfbb66fbbfde9a3d403c437631318656355
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-01 15:00:23 +0100

    another way to create database if already exists

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- This script creates a database in yout MySQL server
+-- if it already exists so as not to  return fail
+-- SELECT and SHOW statements not used
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
