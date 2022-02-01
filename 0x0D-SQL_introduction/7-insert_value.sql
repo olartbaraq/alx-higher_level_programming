8f18b078d193bfa31b8754bd0e0b55c4a50fbcd2
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-01 22:51:12 +0100

    insert a new row in the table (update 8)

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -6,4 +6,4 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best school');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
