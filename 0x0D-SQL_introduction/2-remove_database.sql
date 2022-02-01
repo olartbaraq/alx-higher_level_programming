c123046bfe2087507481fe32fa831f1ebd4e2b37
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-01 15:08:56 +0100

    remove a database even if it exists with fail

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script deletes the database hbtn_0c_0 in your MySQL server
+-- If the database hbtn_0c_0 doesn’t exist, the script will not fail
+-- SELECT or SHOW statements not used
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
