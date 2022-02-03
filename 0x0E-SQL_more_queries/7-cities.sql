98acd4623459986d650d17c7e01dca95bdee2d1c
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-03 21:17:07 +0100

    more constraints with Foreign key(update 1)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -13,7 +13,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
        id INT PRIMARY KEY AUTO_INCREMENT,
-       state_id INT NOT NULL ,
+       state_id INT NOT NULL,
        FOREIGN KEY(state_id) REFERENCES (states) (id),
-       name VARCHAR(256)
+       name VARCHAR(256) NOT NULL
 );
