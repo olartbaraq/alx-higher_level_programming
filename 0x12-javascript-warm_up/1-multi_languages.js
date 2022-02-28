f6565ba6e0b78f0c6f708317cccd4729c4b7ea60
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-28 20:57:58 +0100

    a script that prints lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const jaVas = 'JavaScript is amazing';
+const cProg = 'C is fun';
+const pyThoon = 'Python is cool';
+console.log(cProg);
+console.log(pyThoon);
+console.log(jaVas);
