fe403e18bea36b1f331b626ed99a83fb10c4a35a
Olartbaraq <akanbi.ges201@gmail.com>
2022-02-28 23:37:31 +0100

    arrays and loops to print and fixed semistandard check

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let item = 0; item < 3; item++) {
-    console.log(multiLanguages[item]);
+  console.log(multiLanguages[item]);
 }
