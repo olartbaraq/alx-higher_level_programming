ec53fe7e23380fea03699c2bae6401f4ec6aafb0
Olartbaraq <akanbi.ges201@gmail.com>
2022-03-01 21:54:58 +0100

    sqaure inherits from rectangle

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+	this.width = w;
+	this.height = h;
+    }
+  }
+
+  print () {
+    for (let item = 0; item < this.height; item++) {
+	console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let temp = 0;
+    temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
