8e5a206ef963a89e0d9f0bbdb3b877a5763d2831
Olartbaraq <akanbi.ges201@gmail.com>
2022-03-07 21:21:04 +0100

    first state object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Lists all state objevts from database using sqlalchemy"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    usr = sys.argv[1]
+    pswd = sys.argv[2]
+    db_name = sys.argv[3]
+
+# dialect+driver://username:password@host:port/database
+    c_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(usr, pswd, db_name)
+    engine = create_engine(c_str, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
