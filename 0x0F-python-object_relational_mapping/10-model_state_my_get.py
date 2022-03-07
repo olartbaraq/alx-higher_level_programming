444ea4a403d3ff2a803ee0653e96afe75bdd06b3
Olartbaraq <akanbi.ges201@gmail.com>
2022-03-07 21:58:36 +0100

    get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Lists all state objevts from database using sqlalchemy"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    usr = sys.argv[1]
+    pswd = sys.argv[2]
+    db_name = sys.argv[3]
+    name_to_search = sys.argv[4]
+
+# dialect+driver://username:password@host:port/database
+    c_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(usr, pswd, db_name)
+    engine = create_engine(c_str, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        state = session.query(State).\
+            filter(State.name == name_to_search).first()
+        print(state.id)
+    except Exception:
+        print("Not found")
