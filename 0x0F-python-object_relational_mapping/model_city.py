7d58889ff92cb6db619966721bfddf56b6d3274a
Olartbaraq <akanbi.ges201@gmail.com>
2022-03-08 00:03:02 +0100

    fixed pycodestyle

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,9 +6,10 @@ from model_state import Base
 
 
 class City(Base):
-        """ Class City that inherities from State class"""
-        __tablename__ = 'cities'
-        id = Column(Integer, primary_key=True,
-                    nullable=False, unique=True, autoincrement=True)
-        name = Column(String(128), nullable=False)
-        state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
+
+    """Class City that inherities from State class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
