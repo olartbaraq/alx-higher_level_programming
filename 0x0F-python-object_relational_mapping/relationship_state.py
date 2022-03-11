9c214d0bf45b72a20bd829577d73920ae05e97d5
Olartbaraq <akanbi.ges201@gmail.com>
2022-03-11 22:58:07 +0100

    imported relationship module

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,6 +3,8 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import relationship, backref
+
 
 Base = declarative_base()
 
@@ -10,6 +12,7 @@ Base = declarative_base()
 class State(Base):
     """ Class State that inherities from Base class"""
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
