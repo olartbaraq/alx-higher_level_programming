601ce153875748cfeacd8e28da0d1a5d3d4db9b4
olart <akanbi.ges201@gmail.com>
2022-04-12 17:50:55 +0100

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argument = process.argv[2];
+const siteAddress = String(argument);
+const options = {
+  url: siteAddress,
+  headers: {
+    'User-agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = JSON.parse(body);
+    const filtered = info.results;
+    // console.log(filtered);
+    let count = 0;
+    // since fitered is now an array of dictionary(json)
+    for (let i = 0; i < filtered.length; i++) {
+      const results = filtered[i];
+      const characters = results.characters;
+      // console.log(characters);
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+}
+request(options, callback);
