eabf3602a38e722fe2b1471b750afc958867b1f1
olart <akanbi.ges201@gmail.com>
2022-04-12 12:39:39 +0100

    Read-me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 const argvs = process.argv.slice(2);
-file_path = String(argvs);
+const filePath = String(argvs);
 const fs = require('fs');
 
-fs.readFile( file_path, 'utf-8', (error, data) => {
-    if (error) {
-	console.log(error);
-	return;
-    }
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-    console.log(data.toString());
+  console.log(data.toString());
 });
